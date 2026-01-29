9a1c354c87588d0007041056a9ebf2026d3c8d4a
Malik-xae-12 <az786muzaffar@gmail.com>
2026-01-29 21:27:29 +0530

    Advantage of TS and First TS File

---
+++ basics.ts
@@ -0,0 +1,8 @@
+function greeting(name:string):string{
+    return `Hello ${name}`
+    // return "hello",name        //will run but best practice is to use above one
+}
+
+
+console.log(greeting("hitesh"));
+
